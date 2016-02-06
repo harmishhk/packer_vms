3e4f90f5f3e736285f263947c3eabd1e37d8f3b9
Harmish Khambhaita <harmish@laas.fr>
2016-02-06 18:02:19 +0100

    adding gazebo installation with ros making indigo as default ros repo

--- scripts/ros.sh
+++ scripts/ros.sh
@@ -20,27 +20,60 @@ ros-$ROS_VERSION-navigation \
 python-catkin-tools
 
 ## setup rosdep
-#sudo rosdep init
-#rosdep update
+sudo rosdep init
+rosdep update
 
 if [[ ! "$SPENCER" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]]; then
     echo "==> ros-spencer installation is disabled"
-    exit
+else
+    echo "==> installing spencer related packages for ros $ROS_VERSION"
+
+    sudo apt-get -y install \
+    libmrpt-dev \
+    mrpt-apps \
+    freeglut3-dev \
+    libsvm-dev \
+    libsdl-image1.2-dev \
+    libpcap-dev \
+    libgsl0-dev \
+    ros-$ROS_VERSION-bfl \
+    ros-$ROS_VERSION-control-toolbox \
+    ros-$ROS_VERSION-driver-base \
+    ros-$ROS_VERSION-sound-play \
+    ros-$ROS_VERSION-joy \
+    ros-$ROS_VERSION-yocs-cmd-vel-mux
 fi
 
-echo "==> installing spencer related packages for ros $ROS_VERSION"
+if [[ ! "$GAZEBO" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]]; then
+    echo "==> ros-gazebo installation is disabled"
+else
+    echo "==> installing gazebo and related packages for ros $ROS_VERSION"
 
-sudo apt-get -y install \
-libmrpt-dev \
-mrpt-apps \
-freeglut3-dev \
-libsvm-dev \
-libsdl-image1.2-dev \
-libpcap-dev \
-libgsl0-dev \
-ros-$ROS_VERSION-bfl \
-ros-$ROS_VERSION-control-toolbox \
-ros-$ROS_VERSION-driver-base \
-ros-$ROS_VERSION-sound-play \
-ros-$ROS_VERSION-joy \
-ros-$ROS_VERSION-yocs-cmd-vel-mux
+    # install dependencies
+    sudo apt-get -y install \
+        wget
+
+    # setup source-list and keys
+    sudo sh -c "echo 'deb http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main' > /etc/apt/sources.list.d/gazebo-stable.list"
+    wget http://packages.osrfoundation.org/gazebo.key -O /tmp/gazebo.key
+    sudo apt-key add /tmp/gazebo.key
+    sudo apt-get update
+
+    # install gazebo
+    sudo apt-get -y install \
+        ros-$ROS_VERSION-gazebo6-ros-pkgs \
+        ros-$ROS_VERSION-gazebo6-ros-control
+
+    # install pr2 simulator dependencies
+    sudo apt-get -y install \
+        ros-$ROS_VERSION-pr2-controller-manager \
+        ros-$ROS_VERSION-pr2-msgs \
+        ros-$ROS_VERSION-pr2-dashboard-aggregator \
+        ros-$ROS_VERSION-pr2-controllers
+
+    export TERM=xterm
+    source /opt/ros/$ROS_VERSION/setup.bash
+    git clone -b $ROS_VERSION-devel https://github.com/harmishhk/pr2_simulator.git /tmp/pr2_ws/src/pr2_simulator
+    catkin config --workspace /tmp/pr2_ws -i /opt/ros/$ROS_VERSION --install
+    sudo catkin build --workspace /tmp/pr2_ws
+fi
