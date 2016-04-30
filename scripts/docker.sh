98384881414b8d8eb999bc9f53a5841c257ec7f0
Harmish Khambhaita <harmish@laas.fr>
2016-04-30 21:34:28 +0200

    updating docker installation for 1604 removing 1504 as it isnot supproted by docker anymore

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -11,19 +11,24 @@ fi
 UBUNTU_VERSION=$(lsb_release -rs)
 UBUNTU_MAJOR_VERSION=$(lsb_release -rs | cut -f1 -d .)
 
-# install docker prerequisites
+# install docker prerequisites, add docker key and repository
 sudo apt-get update
-sudo apt-get -y install linux-image-extra-$(uname -r)
-
-# add docker key and repository
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo touch /etc/apt/sources.list.d/docker.list
 if [[ "$UBUNTU_VERSION" =~ "14.04" ]]; then
+    echo "==> installing docker prerequisites" 2>&1 | tee -a $LOGFILE
+    sudo apt-get -y install apt-transport-https ca-certificates linux-image-extra-$(uname -r) apparmor
     sudo sh -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' >> /etc/apt/sources.list.d/docker.list"
 elif [[ "$UBUNTU_VERSION" =~ "15.04" ]]; then
-    sudo sh -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-vivid main' >> /etc/apt/sources.list.d/docker.list"
+    echo "==> docker is no more supported for this ($UBUNTU_VERSION) version of ubuntu" 2>&1 | tee -a $LOGFILE
 elif [[ "$UBUNTU_VERSION" =~ "15.10" ]]; then
+    echo "==> installing docker prerequisites" 2>&1 | tee -a $LOGFILE
+    sudo apt-get -y install apt-transport-https ca-certificates linux-image-extra-$(uname -r)
     sudo sh -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-wily main' >> /etc/apt/sources.list.d/docker.list"
+elif [[ "$UBUNTU_VERSION" =~ "16.04" ]]; then
+    echo "==> installing docker prerequisites" 2>&1 | tee -a $LOGFILE
+    sudo apt-get -y install apt-transport-https ca-certificates linux-image-extra-$(uname -r)
+    sudo sh -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' >> /etc/apt/sources.list.d/docker.list"
 else
     echo "==> docker will not be installed for this ($UBUNTU_VERSION) version of ubuntu" 2>&1 | tee -a $LOGFILE
     exit
