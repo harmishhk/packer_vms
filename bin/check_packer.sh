7a67b3ebdd22b44573be4c56f9366968f7c2501b
Harmish Khambhaita <harmish@laas.fr>
2016-06-06 11:29:38 +0200

    fixing packer download link

--- bin/check_packer.sh
+++ bin/check_packer.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-set -e
+#!/bin/sh
 
 # check if packer is installed
 if type packer >/dev/null 2>&1; then
@@ -9,8 +7,8 @@ elif type .packer/packer >/dev/null 2>&1; then
     echo "found packer..."
     export PATH=$PATH:`pwd`/.packer
 else
-    echo "getting packer...";
-    wget -q https://dl.bintray.com/mitchellh/packer/packer_0.10.1_linux_amd64.zip
+    echo "getting packer..."
+    wget -q https://releases.hashicorp.com/packer/0.10.1/packer_0.10.1_linux_amd64.zip
     unzip -q packer_0.10.1_linux_amd64.zip -d .packer
     rm packer_0.10.1_linux_amd64.zip
     export PATH=$PATH:`pwd`/.packer
