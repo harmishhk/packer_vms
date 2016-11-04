25da2ba320721537993c3552acafba2626ca1569
harmish khambhaita <harmish@laas.fr>
2016-11-04 12:35:56 +0100

    updating packer version

--- bin/check_packer.sh
+++ bin/check_packer.sh
@@ -8,8 +8,8 @@ elif type .packer/packer >/dev/null 2>&1; then
     export PATH=$PATH:`pwd`/.packer
 else
     echo "getting packer..."
-    wget -q https://releases.hashicorp.com/packer/0.10.1/packer_0.10.1_linux_amd64.zip
-    unzip -q packer_0.10.1_linux_amd64.zip -d .packer
-    rm packer_0.10.1_linux_amd64.zip
+    wget -q https://releases.hashicorp.com/packer/0.11.0/packer_0.11.0_linux_amd64.zip
+    unzip -q packer_0.11.0_linux_amd64.zip -d .packer
+    rm packer_0.11.0_linux_amd64.zip
     export PATH=$PATH:`pwd`/.packer
 fi
